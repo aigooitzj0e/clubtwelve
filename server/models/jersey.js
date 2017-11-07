ee6e58e9b243e98669ab05c04d379564028ea3e0
Joe Yu <joeyu@Joes-MacBook-Pro.local>
2017-11-07 13:12:51 -0800

    models added

---
+++ server/models/jersey.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let JerseySchema = mongoose.Schema({
+    player: {type: String, required: true},
+	team: {type:String, required:true},
+	description: {type:String, required:true, maxlength: 200},
+	price: {type: Number, required:true, min: 1},
+
+}, {timestamps:true});
+mongoose.model('Jersey', JerseySchema);
---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let UserSchema = mongoose.Schema({
+	name: {type:String, required:true},
+	email: {type: String, required:true},
+	password: {type:String, required:true, minlength: 8},
+});
+
+
+mongoose.model('User', UserSchema);
